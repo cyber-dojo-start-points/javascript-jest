a0ad947c734892a4dc88058fb8940b920f6992bc
JonJagger <jon@kosli.com>
2026-09-03 08:22:28 +0100

    Lint the sandbox by naming it, not by a glob that happens to match it

--- start_point/cyber-dojo.sh
+++ start_point/cyber-dojo.sh
@@ -3,5 +3,5 @@ ln -s /etc/jest/node_modules ${CYBER_DOJO_SANDBOX}/node_modules
 
 # Calling [npm run ...] is sloooow so don't to that.
 
-node_modules/.bin/eslint --config ${CYBER_DOJO_SANDBOX}/eslint.config.js /**/*.js
+node_modules/.bin/eslint --config ${CYBER_DOJO_SANDBOX}/eslint.config.js ${CYBER_DOJO_SANDBOX}
 node_modules/.bin/jest --coverage
